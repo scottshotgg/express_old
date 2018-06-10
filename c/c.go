597b3883af963da1e39f3004b985a9940e4847f2
Scott Gaydos <scott.gaydos@yum.com>
2018-06-10 04:24:15 -0500

    shit

--- c/c.go
+++ c/c.go
@@ -11,6 +11,15 @@ import (
 	"github.com/scottshotgg/Express/token"
 )
 
+// Anonymous Struct Declaration
+// struct thing{
+// 	std::string type;
+// 	int amount;
+// } thingy = {
+// 	"hey",
+// 	6,
+// };
+
 // This is a placeholder for the C converter package that will be used to convert Express -> C
 // Doing so will allow Express to leverage all available C tools
 
@@ -25,6 +34,18 @@ var (
 	r   *rand.Rand
 	f   *os.File
 	err error
+
+	typeMap = map[string]int{
+		"int":    1,
+		"string": 2,
+		"bool":   3,
+		"float":  4,
+		"char":   5,
+		"object": 6,
+		"string": 7,
+		// don't know if I'll need this
+		"int[]": 8,
+	}
 )
 
 // class MyFieldInterface
@@ -50,14 +71,44 @@ func translateObject(t token.Token) {
 
 	mapValue := func() (valueString string) {
 		for k, v := range trueValue {
-
 			switch v.Type {
+			// TODO: dont feel like doing this rn, something with the quotes and checking string type w/e
+			// case "var":
+			// // int abc = 5;
+			// // Any zyx = Any{ "int", &abc };
+			// varName := k + strconv.Itoa(int(r.Uint32()))
+			// thing := strings.Join([]string{v.Acting, varName, "=", fmt.Sprintf("%v", v.True)}, " ") + ";\n"
+			// thing += "Any " + k + " = Any{ \"" + v.Acting + "\", &" + varName + "};\n"
+			// fmt.Println(thing)
+			// _, err = f.Write([]byte(thing))
+			// if err != nil {
+			// 	fmt.Println("error writing to file")
+			// 	os.Exit(9)
+			// }
+
+			case "object":
+				v.Name = k
+				translateObject(token.Token{
+					Type:  "VAR",
+					Value: v,
+				})
+				valueString += t.Value.Name + "[\"" + k + "\"] = Any{ \"" + v.Type + "\", &" + v.Name + " };\n"
+
+			case "array":
+				v.Name = k
+				translateArray(token.Token{
+					Type:  "VAR",
+					Value: v,
+				})
+				valueString += t.Value.Name + "[\"" + k + "\"] = Any{ \"" + v.Type + "\", &" + v.Name + " };\n"
+
 			case "string":
 				// randomize the name of the integer
 				varName := k + strconv.Itoa(int(r.Uint32()))
 				valueString += "std::" + v.Type + " " + varName + " = " + fmt.Sprintf("\"%v\"", v.True) + ";\n"
 				// valueString += "\n" + t.Value.Name + "[\"" + k + "\"] = Any{ \"" + v.Type + "\", (" + v.Type + "*)" + fmt.Sprintf("%v", v.True) + " };"
 				valueString += t.Value.Name + "[\"" + k + "\"] = Any{ \"" + v.Type + "\", &" + varName + " };\n"
+
 			default:
 				// randomize the name of the integer
 				varName := k + strconv.Itoa(int(r.Uint32()))
@@ -73,6 +124,29 @@ func translateObject(t token.Token) {
 	fmt.Println("mapValue", mapValue)
 
 	f.Write([]byte(mapValue))
+}
+
+func translateArray(t token.Token) {
+	fmt.Println(t)
+	trueValue := t.Value.True.([]token.Value)
+	// assuming only single type arrays until I have time to do multi type arrays in C
+	arrayType := trueValue[0].Type
+	arrayValue := func() (valueString string) {
+		for i, v := range trueValue {
+			valueString += fmt.Sprintf("%v", v.True)
+			if i != len(trueValue)-1 {
+				valueString += ", "
+			}
+		}
+		return
+	}()
+	thing := arrayType + " " + t.Value.Name + "[] = { " + arrayValue + " };\n"
+	fmt.Println(thing)
+	_, err = f.Write([]byte(thing))
+	if err != nil {
+		fmt.Println("error writing to file")
+		os.Exit(9)
+	}
 }
 
 func translateVariableStatement(t token.Token) {
@@ -92,32 +166,11 @@ func translateVariableStatement(t token.Token) {
 				os.Exit(9)
 			}
 
-		case "array":
-			trueValue := t.Value.True.([]token.Value)
-
-			// assuming only single type arrays until I have time to do multi type arrays in C
-			arrayType := trueValue[0].Type
-			arrayValue := func() (valueString string) {
-				for i, v := range trueValue {
-					valueString += fmt.Sprintf("%v", v.True)
-					if i != len(trueValue)-1 {
-						valueString += ", "
-					}
-				}
-
-				return
-			}()
-
-			thing := arrayType + " " + t.Value.Name + "[] = { " + arrayValue + " };\n"
-			fmt.Println(thing)
-			_, err = f.Write([]byte(thing))
-			if err != nil {
-				fmt.Println("error writing to file")
-				os.Exit(9)
-			}
-
 		case "object":
 			translateObject(t)
+
+		case "array":
+			translateArray(t)
 
 		// In the case of the object we need to essentially instantiate a struct that will be used even if only temporarily
 		// could just use that json library for now but wtf
--- main.expr.cpp
+++ main.expr.cpp
@@ -2,17 +2,25 @@
 #include <string>
 struct Any { std::string type; void* data; };
 int main() {
-std::map<std::string, Any>obj;
-std::string thing4176209394 = "hey its me";
-obj["thing"] = Any{ "string", &thing4176209394 };
-int a1308182024 = 5;
-obj["a"] = Any{ "int", &a1308182024 };
-float b1143115524 = 67.8;
-obj["b"] = Any{ "float", &b1143115524 };
-int bb = 11;
 int arrayBrah[] = { 5, 5 };
 float thing = 77;
-int me3923516135 = 5;
-Any me = Any{ "int", &me3923516135};
+int me3035228544 = 5;
+Any me = Any{ "int", &me3035228544};
 std::string hey = "its me";
+std::map<std::string, Any>obj;
+std::map<std::string, Any>anotherOne;
+int a1275390956 = 7;
+anotherOne["a"] = Any{ "int", &a1275390956 };
+int arrayMeUpScotty[] = { 7, 8, 9 };
+int a1537738808 = 5;
+obj["a"] = Any{ "int", &a1537738808 };
+float b2045175289 = 67.8;
+obj["b"] = Any{ "float", &b2045175289 };
+std::string thing2493503111 = "hey its me";
+obj["thing"] = Any{ "string", &thing2493503111 };
+bool notMe1441537250 = false;
+obj["notMe"] = Any{ "bool", &notMe1441537250 };
+obj["anotherOne"] = Any{ "object", &anotherOne };
+obj["arrayMeUpScotty"] = Any{ "array", &arrayMeUpScotty };
+int bb = 11;
 }
